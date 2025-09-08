01aac24ffb11f38b3a596a08b4ba1fc7725727f6
Marcela1723 <nashly15@hotmail.com>
2025-09-07 21:32:53 -0500

    creacion de archivo de datos

--- archivo.js
+++ archivo.js
@@ -1,26 +1,3 @@
-const datos = [
-  {
-    causalidad: 'Bajar Auditoria,  Log de errores, Silent Debug, configuracion por solicitud del cliente o de NCR',
-    tipoSR: 'CHGC',
-    codigoReparacion: '34',
-    categoria: '1B',
-    mayor: '50',
-    menor: '1O',
-    codigoEntorno: '19KI',
-    description: ''
-  },
-  {
-    causalidad: 'Bajar Auditoria,  Log de errores, Silent Debug, configuracion por solicitud del cliente o de NCR',
-    tipoSR: 'CHGC',
-    codigoReparacion: '34',
-    categoria: '1B',
-    mayor: '50',
-    menor: '1O',
-    codigoEntorno: '19KI',
-    description: ''
-  }
-]
-
 // Buscar datos por nombre
 function buscarDatos() {
   const query = document.getElementById('searchInput').value.trim().toLowerCase();
---
+++ datos.js
@@ -0,0 +1,22 @@
+const datos = [
+  {
+    causalidad: 'Bajar Auditoria,  Log de errores, Silent Debug, configuracion por solicitud del cliente o de NCR',
+    tipoSR: 'CHGC',
+    codigoReparacion: '34',
+    categoria: '1B',
+    mayor: '50',
+    menor: '1O',
+    codigoEntorno: '19KI',
+    description: ''
+  },
+  {
+    causalidad: 'Bajar Auditoria,  Log de errores, Silent Debug, configuracion por solicitud del cliente o de NCR',
+    tipoSR: 'CHGC',
+    codigoReparacion: '34',
+    categoria: '1B',
+    mayor: '50',
+    menor: '1O',
+    codigoEntorno: '19KI',
+    description: ''
+  }
+]
