c3929d05dac00638fe6fc9a813d1a3dc897a36ba
Marcela1723 <nashly15@hotmail.com>
2025-09-07 21:41:45 -0500

    separacion de los resultados

--- archivo.js
+++ archivo.js
@@ -1,18 +1,22 @@
 // Buscar datos por nombre
 function buscarDatos() {
-  const query = document.getElementById('searchInput').value.trim().toLowerCase();
+  const query = document.getElementById('busqueda-input').value.trim().toLowerCase();
   const resultados = datos.filter(d => d.causalidad.toLowerCase().includes(query));
-  const resultsDiv = document.getElementById('results');
+  const resultsDiv = document.getElementById('resultados');
 
   resultsDiv.innerHTML = resultados.length
-    ? resultados.map(d => `<p><strong>Causalidad: ${d.causalidad}</strong></p>
-      Tipo SR: ${d.tipoSR}</p>
-      Código de Reparación: ${d.codigoReparacion}</p>
-      Categoría: ${d.categoria}</p>
-      Mayor: ${d.mayor}</p>
-      Menor: ${d.menor}</p>
-      Código Entorno: ${d.codigoEntorno}</p>nombre
-      Descripción: ${d.descripcion}</p>`).join('')
+    ? resultados.map(d => `
+      <div class="resultado">
+        <p><strong>Causalidad: ${d.causalidad}</strong></p>
+        <p>Tipo SR: ${d.tipoSR}</p>
+        <p>Código de Reparación: ${d.codigoReparacion}</p>
+        <p>Categoría: ${d.categoria}</p>
+        <p>Mayor: ${d.mayor}</p>
+        <p>Menor: ${d.menor}</p>
+        <p>Código Entorno: ${d.codigoEntorno}</p>
+        <p>Descripción: ${d.descripcion}</p>
+      </div>
+      `).join('')
     : '<p>No se encontraron resultados.</p>';
 }
 
