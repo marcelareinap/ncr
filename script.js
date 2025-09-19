8d5a4d7df4041564a84360ff58e5498d96def0e1
Marcela1723 <nashly15@hotmail.com>
2025-09-18 23:25:04 -0500

    paso de decisiones

--- script.js
+++ script.js
@@ -10,29 +10,100 @@ function ocultarPaso(paso) {
     seleccionarPaso(paso).classList.remove('active');
 }
 
+function seleccionarMotivo(motivo) {
+    return document.querySelector(`.${motivo}`);
+}
+
+function mostrarMotivo(motivo) {
+    seleccionarMotivo(motivo).classList.add('active');
+}
+
+function ocultarMotivo(motivo) {
+    seleccionarMotivo(motivo).classList.remove('active');
+}
+
+let estadoOperativo = '';
+
 function respuesta(dato, paso) {
     console.log(dato, paso);
 
+
+    // paso 1
     if (dato === 'site'){
         mostrarPaso('step2');
         ocultarPaso('step4');
-
     }
 
     if (dato === 'remote'){
         ocultarPaso('step2');
         ocultarPaso('step3');
         mostrarPaso('step4');
+
+
+
+        mostrarMotivo('motivo1');
+        mostrarMotivo('motivo2');
+        mostrarMotivo('motivo3');
+
     }
 
-
+    // Paso 2
     if (dato === 'operativo' || dato === 'noOperativo'){
         mostrarPaso('step3');
+        estadoOperativo = dato;
+        ocultarPaso('step4');
     }
 
-    if (dato === 'suspendido' || dato === 'esperando'){
-        mostrarPaso('step4');
+
+    function resetMotivos(){
+        ocultarMotivo('motivo1');
+        ocultarMotivo('motivo2');
+        ocultarMotivo('motivo3');
     }
+
+    // suspendido = no puedo esperar
+
+    if (dato === 'suspendido'){
+        mostrarPaso('step4');
+        resetMotivos();
+
+        if(estadoOperativo === 'operativo'){
+            console.log('muestro 3 razones');
+            mostrarMotivo('motivo1');
+            mostrarMotivo('motivo2');
+            mostrarMotivo('motivo3');
+        }
+
+        if(estadoOperativo === 'noOperativo'){
+            console.log('muestro 3 razones');
+            mostrarMotivo('motivo1');
+            mostrarMotivo('motivo2');
+            mostrarMotivo('motivo3');
+        }
+    }
+
+    if (dato === 'esperando'){
+        mostrarPaso('step4');
+        resetMotivos();
+        
+        if(estadoOperativo === 'operativo'){
+            console.log('muestro 2 razones');
+            mostrarMotivo('motivo1');
+            mostrarMotivo('motivo2');
+            
+        }
+        if(estadoOperativo === 'noOperativo'){
+            console.log('muestro 1 razon');
+            mostrarMotivo('motivo3');
+        }
+    }
+
+
+
+
+
+
+
 
 
 /*    if (paso){
